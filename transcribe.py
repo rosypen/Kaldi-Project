e08c240c83de89a2097110fe32a911977fc5db74
rosypen <rosy.pen.1@gmail.com>
2022-06-05 04:05:40 -0700

    Simple attempt to transcribe audio files.
    Basically as of now this script can
    - create an eaf file w/ same name as audio file
    - add an annotation to the eaf file with the word spoken in that file.
    
    Elpis documentation mentions it's okay for transcriptions to not be
    at the individual sound level or even word level.
    (https://elpis.readthedocs.io/en/latest/wiki/preparing-files.html)
    
    But I think at the very least we should try to get the beginning of the
    utterance correct.

---
+++ transcribe.py
@@ -0,0 +1,57 @@
+# Author: Rosetta Pendleton
+# This file is for creating transcription files (eaf) for the audio clips provided by Mozilla Common Voice
+# https://commonvoice.mozilla.org/en/datasets
+# Eaf files can have multiple tiers of annotation (eg. phonetic pronunciation, orthographic form, gloss, etc.)
+# We could potentially have tier 0 be the orthorgraphic transcription and tier 1 be the corresponding digit
+
+from read_tsv import *
+import pympi
+
+import logging
+
+logging.basicConfig(level = logging.INFO)
+
+def main(lang:str):
+     '''
+     lang: should be one of [cy|br|de]
+     '''
+     train_tsv = 'datasets/{}/tsv/train.tsv'.format(lang)
+     digit_file = 'datasets/{}/welsh_digits.txt'.format(lang)
+     train_df = load_data(train_tsv, digit_file)
+
+     # Train_files.txt is just a text file that has the names of all the audio files in the train set.
+     # Eg. common_voice_cy_22287428.wav
+     # The eaf file will have the same name except .eaf instead of .wav
+     train_file = 'datasets/{}/train_files.txt'.format(lang)
+     logging.info("transcribing train set from: {}".format(train_file))
+
+     train_clips = open(train_file, 'r')
+     for clip_name in train_clips.readlines():
+          eaf_name = clip_name.replace(".wav\n", ".eaf")
+          eaf_file = 'datasets/{}/transcribed/{}'.format(lang, eaf_name)
+
+          # Create an eaf object
+          eaf = pympi.Elan.Eaf()
+          eaf.add_language(lang)
+
+          # Grab word for this clip from the dataframe
+          mp3_name = clip_name.replace(".wav\n", ".mp3")
+          result_df = train_df[train_df['path'] == mp3_name]
+          word = result_df.at[0,'sentence']
+
+          # Add annotations to the eaf object
+          # NOTE: add_annotation(id_tier, start, end, value='', svg_ref=None); start/end timestamps need to be in miliseconds
+          # TODO: Find where the utterance starts in the audio file.
+          eaf.add_tier('Phrase') # I think I am also supposed to add a linguistic type??? but not sure what those are.
+          eaf.add_annotation('Phrase', 0, 1000, word)
+          # So ideally, we would also have each sound/letter in the word annotated to the millisecond
+          # eaf.add_tier('Segment')
+          # for sound in word:
+          #    eaf.add_annotation('Segment', start, end, sound)
+          
+          # Write the eaf object to a file.
+          eaf.to_file(eaf_file)
+          break
+
+if __name__ == "__main__":
+    main("cy")
