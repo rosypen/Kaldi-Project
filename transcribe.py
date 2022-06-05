5d920d68a9f891efc1bbf881d382c381c78107fe
rosypen <rosy.pen.1@gmail.com>
2022-06-05 14:11:45 -0700

    Update: Welsh not missing train transcriptions afterall

--- transcribe.py
+++ transcribe.py
@@ -26,7 +26,9 @@ def main(lang:str):
      logging.info("transcribing train set from: {}".format(train_file))
 
      train_clips = open(train_file, 'r')
-     for clip_name in train_clips.readlines():
+     lines = train_clips.readlines()
+     missing = 0
+     for clip_name in lines:
           eaf_name = clip_name.replace(".wav\n", ".eaf")
           eaf_file = 'datasets/{}/transcribed/{}'.format(lang, eaf_name)
 
@@ -37,11 +39,12 @@ def main(lang:str):
           # Grab word for this clip from the dataframe
           mp3_name = clip_name.replace(".wav\n", ".mp3")
           result_df = train_df[train_df['path'] == mp3_name]
+          i = result_df.index[0]
           try:
-               word = result_df.at[0,'sentence']
-          except:
+               word = result_df.at[i,'sentence']
+          except Exception as e:
                logging.info("Couldn't find the word for {} in the tsv file".format(mp3_name))
-               continue
+               missing +=1
 
           # Add annotations to the eaf object
           # NOTE: add_annotation(id_tier, start, end, value='', svg_ref=None); start/end timestamps need to be in miliseconds
@@ -56,6 +59,7 @@ def main(lang:str):
           # Write the eaf object to a file.
           eaf.to_file(eaf_file)
           break
+     logging.info("Missing {} transcriptions of {} files".format(missing, len(lines)))
 
 if __name__ == "__main__":
     main("cy")
